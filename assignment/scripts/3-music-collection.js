ea6c3f89b9e5acd6f45d4fe4200b3af5e3e43990
Maxwell Chrysler <maxwellchrysler@hotmail.com>
2023-05-03 19:14:25 -0500

    created function addToCollection and showCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,20 +2,30 @@ console.log('***** Music Collection *****')
 
 let collection = []; // Create collection array, starts empty
 
-function addToCollection(){
-    title: ,
-    artist: ,
-    yearPublished: ,
-
-
-    console.log(collection);
+function addToCollection(title, artist, yearPublished){ // Have to have room for the three seperate things
+    let musicInfo = { title, artist, yearPublished } // I forgot if this is proper formatting but it seemed like the most normal to do
+    collection.push(musicInfo); // pushing each entry into the collection array
+    return musicInfo;
 }
 
 
+addToCollection("Give yourself a try","a brief inquiry into online relationships","2018");
+addToCollection("Blessings ft. (Ty Dolla$)","Coloring book","2016");
+addToCollection("Derezzed","Daft Punk , Avicii ","2011");
+addToCollection("Desperado","The Eagles","1973");
+addToCollection("Everybody wants to rule the world","Tears for fears","1985");
+addToCollection("Hollywood baby","100 Gecs","2023");
 
 
+console.log(collection); // in the console log there is thing called prototype and Im not sure what any of it is trying to tell me
 
+function showCollection(musicInfoArray){ // 
+  for (let i=0; i < musicInfoArray.length; i++){ // making sure I use let for these for loops from now on
+    console.log( musicInfoArray[i].title,'By', musicInfoArray[i].artist,'published in', musicInfoArray[i].yearPublished);
 
+  } 
+}
+showCollection(collection);
 
 
 /* - Create a variable `collection` that starts as an empty array.
@@ -25,6 +35,8 @@ function addToCollection(){
   - Create a new object having the above properties
   - Add the new object to the end of the `collection` array
   - Return the newly created object
+
+  
 
 - Test the `addToCollection` function:
   - Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
@@ -59,4 +71,4 @@ function findByArtist(){
 
 - Test the `findByArtist` function. Make sure to test with an artist you know is in the collection, as well as an artist you know is not in your collection. Check that for artists with multiple matches, all are found.
 
-> When testing your functions, write all tests in the JavaScript file!
+> When testing your functions, write all tests in the JavaScript file! */
